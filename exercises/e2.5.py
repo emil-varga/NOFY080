a4ba5b80d44ac0526f65d1d030305d1ad4850f41
Emil Varga <varga.emil@gmail.com>
2025-09-30 15:51:34 +0200

    New exercise, added Marek to authors, and minor changes.

--- exercises/e2.5.py
+++ exercises/e2.5.py
@@ -1,63 +1,51 @@
-class Fraction:
-    """ Represents a/b """
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b
+import time # to measure time
 
-    # the __something__ methods are called magic or dunder (Double UNDERscore)
-    # they indicate some built-in syntax, for example, if f1 and f2 are Fractions
-    #f1 + f2 is translated to f1.__add__(f2)
-    def __add__(self, other):
-        return Fraction(self.a*other.b + self.b*other.a,
-                        self.b*other.b)
+# Recursive implementation of the Fibonacci sequence, including a docstring
+def fib(k): #triple quote """ indicates a string which spans multiple lines
+    """Calculates the k-th Fibonacci number using a recursive algorithm
     
-    #f1 - f2 -> f1.__sub__(f2)
-    def __sub__(self, other):
-        return Fraction(self.a*other.b - self.b*other.a,
-                        self.b*other.b)
+    Parameters
+    ---------
+    k : positive integer
+        The Fibonacci number to calculate  
     
-    #f1 * f2 -> f1.__mul__(f2)
-    def __mul__(self, other):
-        return Fraction(self.a*other.a, self.b*other.b)
+    Returns
+    -----
+    positive integer
+         k-th Fibonacci number
     
-    #f1 / f2 -> f1.__truediv__(f2)
-    def __truediv__(self, other):
-        return Fraction(self.a*other.b, self.b*other.a)
-    
-    def value(self):
-        return self.a/self.b
-    
-    # str(f1) -> f1.__str__(); used in print()
-    #textual representation of the object
-    def __str__(self):
-        return f"{self.a}/{self.b}"
-    #__repr__ is similar to __str__, except it
-    #gets called when we simply evaluate the object
-    #in REPL
-    def __repr__(self):
-        return str(self)
-    
-    #f1(args) -> f1.__call__(args)
-    def __call__(self):
-        return self.value()
-    
-    #f1[idx] -> f1.__getitem__(idx)
-    def __getitem__(self, idx):
-        if idx == 0:
-            return self.a
-        elif idx == 1:
-            return self.b
-        else:
-            raise ValueError
+    """
+    if k < 3: #boundary condition, F_1 = 1, F_2 = 1
+        return 1
+    return fib(k-1) + fib(k-2)
 
-if __name__ == '__main__':
-    f1 = Fraction(1,2)
-    f2 = Fraction(3,4)
-    
-    print(f1 + f2) # === f1.__add__(f2)
-    print(f1.value())
-    print(f1.__str__())
-    print((f1 + f2).value())
-    print(f1.value() + f2.value())
-    print(f1())
-    print(f1[0])
+def fibi(k): # iterative implementation
+    #if the docstring is short, simple quotes are enough
+    "Calculate the k-th Fibonacci number using iterations." 
+    if k < 3:
+        return 1
+    fk_1 = 1 #F_(k-1)
+    fk_2 = 1 #F_(k-2)
+    for k in range(2, k):
+        fk = fk_1 + fk_2
+        fk_2 = fk_1
+        fk_1 = fk
+    return fk
+
+#function time.time() returns the number of seconds (with microsecond resolution) from
+#midnight 1.1.1970 (the so-called Unix Epoch), which, by itself, is not particularly useful
+#but the difference between two calls to time.time() gives the elapsed time
+
+t0_recursion = time.time() #time stamp just before the calculation
+f_rec = fib(20)      #calculate the 20th Fk
+t1_recursion = time.time() #time stamp at the end
+total_time_rec = t1_recursion - t0_recursion
+
+#similarly for iterations
+t0_iter = time.time() 
+f_iter = fibi(20)
+t1_iter = time.time()
+total_time_iter = t1_iter - t0_iter
+
+print(f"Recursive: {f_rec}, time: {total_time_rec} s.")
+print(f"Iterative: {f_iter}, time: {total_time_iter} s.")
